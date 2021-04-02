bd9d18e9a714eba8fc571c3d1c0c1bfec5c758b9
nayerdi <nicolinoayerdi@gmail.com>
2021-04-02 20:57:39 -0300

    agrega cards styleados para mostrar personajes

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,19 +1,54 @@
 import React from 'react'
+import './CharacterList.css'
 
 const CharacterList = (props) => {
-	const characters = props.characters.map(( { id, name, status, created }) => {
+	const characters = props.characters.map(( { id, name, status, created, image, species, gender, location}) => {
+
+		let statusStyle = status === 'Dead' ? "dead" : '';
+		let genderStyle = getGenderIcon();
+		let createdString = formatDate();
+
 		return (
-			<div className="item" key={id}>
+			<div key={id} className="ui card">
+				<div className="image"><img className={statusStyle} src={image} alt="no img"/></div>
 				<div className="content">
-					<span className="header">{name}</span>
+					<div className="header">{name}</div>
 					<div className="description">{status}</div>
+					<div className="meta"><span className="date">{createdString}</span></div>
+				</div>
+				<div className="extra content">
+					<div>
+						<i aria-hidden={true} className={genderStyle}/>
+						{gender}
+					</div>
+					<div>
+						<i aria-hidden={true} className="location arrow icon"/>
+						{location.name}
+					</div>
+					<div>
+						<i aria-hidden={true} className="users icon"/>
+						{species}
+					</div>
 				</div>
 			</div>
 		);
+
+		function formatDate() {
+			const creationDate = new Date(created);
+			return `${creationDate.getDate()}/${creationDate.getMonth()}/${creationDate.getFullYear()}`;
+		}
+
+		function getGenderIcon() {
+			if (gender === 'Male')
+				return 'male icon';
+			if (gender === 'Female')
+				return 'female icon';
+			return '';
+		}
 	});
 
 	return (
-		<div className="ui list">
+		<div className="ui cards">
 			{characters}
 		</div>
 	);
