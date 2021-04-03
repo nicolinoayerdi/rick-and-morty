4de3d4086c43008219f2f7226f9627392551b011
nayerdi <nicolinoayerdi@gmail.com>
2021-04-03 12:48:01 -0300

    Pagination

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,49 @@ import React from 'react';
 import rickmorty from "../api/rickmorty";
 import CharacterList from "./CharacterList";
 import 'semantic-ui-css/semantic.min.css';
+import './App.css'
+import {Pagination} from "semantic-ui-react";
 
 
 class App extends React.Component {
 
-	state = { characters: [] }
+	state = { characters: [], info: { count: 0, pages: 0} };
 
-	fun = async () => {
+	fun = async (page) => {
+		console.log('fun', page);
 		const a = await rickmorty.get('character', {
 			params: {
-				page: 1
+				page: page
 			}
 		});
-		this.setState({ characters: a.data.results });
+		this.setState({ characters: a.data.results, info: a.data.info });
 
 	}
 
+	componentDidMount() {
+		this.fun(1).then();
+	}
+
 	render() {
-		if (this.state.characters.length === 0) {
+		/*if (this.state.characters.length === 0) {
 			return <button onClick={this.fun}>GET</button>
-		}
+		}*/
 		return (
 		  <div>
-			  <CharacterList characters={this.state.characters}/>
+			  <h1>RICK AND MORTY CHARACTERS</h1>
+			  <div className="header">
+				  <Pagination
+					  style={{'height': '30px'}}
+					  onPageChange={(a, v) => this.fun(v.activePage)}
+					  defaultActivePage={1}
+					  totalPages={this.state.info.pages}
+					  ellipsisItem={null}
+					  siblingRange={3}
+					  boundaryRange={0}
+				  />
+			  </div>
+			  <CharacterList characters={this.state.characters} info={this.state.info}/>
+
 		  </div>
         );
 	}
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import './CharacterList.css'
+import { Pagination } from "semantic-ui-react"
 
 const CharacterList = (props) => {
 	const characters = props.characters.map(( { id, name, status, created, image, species, gender, location}) => {
 
-		let statusStyle = status === 'Dead' ? "dead" : '';
+		let statusStyle = status === 'Dead' ? 'dead' : '';
 		let genderStyle = getGenderIcon();
 		let createdString = formatDate();
 
 		return (
-			<div key={id} className="ui card">
+
+			<div key={id} className="container-row">
+				<div><img src={image} className={statusStyle}/></div>
+				<div className="details">
+					<div className="container-column">
+						<div className="header">{name}</div>
+						<div className="description">
+							<div>{status}</div>
+						</div>
+					</div>
+					<div className="container-column">
+						<div>{createdString}</div>
+						<div>{species}</div>
+					</div>
+					<div className="container-column">
+						<div>{location.name}</div>
+						<div>{gender}</div>
+					</div>
+				</div>
+			</div>
+
+
+
+			/*<div key={id} className="ui card">
 				<div className="image"><img className={statusStyle} src={image} alt="no img"/></div>
 				<div className="content">
 					<div className="header">{name}</div>
@@ -30,7 +54,7 @@ const CharacterList = (props) => {
 						{species}
 					</div>
 				</div>
-			</div>
+			</div>*/
 		);
 
 		function formatDate() {
@@ -48,8 +72,9 @@ const CharacterList = (props) => {
 	});
 
 	return (
-		<div className="ui cards">
+		<div role="list" className="list-container">
 			{characters}
+			<div style={{'marginTop': '16px'}}>Count: {props.info.count}</div>
 		</div>
 	);
 };
